5b930df88403cd7f3f8017e935e1a90c21ba31ed
Cornolti <melanie.cornolti@orange.fr>
2020-10-01 08:58:04 +0200

    Initialisation de archive-voiture.php

---
+++ archive-voiture.php
@@ -0,0 +1,19 @@
+<?php get_header(); ?>
+<div class="voitures">
+    <?php if (have_posts()) : ?>
+    <?php while (have_posts()) : the_post(); ?>
+    <article class="voiture">
+        <?php the_post_thumbnail( 'thumbnail' ); ?>
+        <h1 class="title">
+            <a href="<?php the_permalink(); ?>">
+                <?php the_title(); ?>
+            </a>
+        </h1>
+        <div class="content">
+            <?php the_content(); ?>
+        </div>
+    </article>
+    <?php endwhile; ?>
+    <?php endif; ?>
+</div>
+<?php get_footer(); ?>
