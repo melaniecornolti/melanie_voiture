9be52e81294bba172d6c80204af4a3d45108bb09
Cornolti <melanie.cornolti@orange.fr>
2020-10-01 08:48:45 +0200

    Modification functions.php

--- functions.php
+++ functions.php
@@ -1,4 +1,12 @@
 <?php
+
+add_theme_support('post-thumbnails');
+add_action('init', 'my_custom_init');
+
+function my_custom_init() {
+	/* notre code PHP pour rajouter les custom post type */
+}
+
 /**
  * melanie_voiture functions and definitions
  *
@@ -7,102 +15,90 @@
  * @package melanie_voiture
  */
 
-if ( ! defined( '_S_VERSION' ) ) {
+if ( ! defined('_S_VERSION')) {
 	// Replace the version number of the theme on each release.
-	define( '_S_VERSION', '1.0.0' );
+	define('_S_VERSION', '1.0.0');
 }
 
-if ( ! function_exists( 'melanie_voiture_setup' ) ) :
-	/**
+if ( ! function_exists('melanie_voiture_setup')) :
+
+/**
 	 * Sets up theme defaults and registers support for various WordPress features.
 	 *
 	 * Note that this function is hooked into the after_setup_theme hook, which
 	 * runs before the init hook. The init hook is too late for some features, such
 	 * as indicating support for post thumbnails.
 	 */
-	function melanie_voiture_setup() {
-		/*
+function melanie_voiture_setup() {
+	/*
 		 * Make theme available for translation.
 		 * Translations can be filed in the /languages/ directory.
 		 * If you're building a theme based on melanie_voiture, use a find and replace
 		 * to change 'melanie_voiture' to the name of your theme in all the template files.
 		 */
-		load_theme_textdomain( 'melanie_voiture', get_template_directory() . '/languages' );
+	load_theme_textdomain('melanie_voiture', get_template_directory() . '/languages');
 
-		// Add default posts and comments RSS feed links to head.
-		add_theme_support( 'automatic-feed-links' );
+	// Add default posts and comments RSS feed links to head.
+	add_theme_support('automatic-feed-links');
 
-		/*
+	/*
 		 * Let WordPress manage the document title.
 		 * By adding theme support, we declare that this theme does not use a
 		 * hard-coded <title> tag in the document head, and expect WordPress to
 		 * provide it for us.
 		 */
-		add_theme_support( 'title-tag' );
+	add_theme_support('title-tag');
 
-		/*
+	/*
 		 * Enable support for Post Thumbnails on posts and pages.
 		 *
 		 * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
 		 */
-		add_theme_support( 'post-thumbnails' );
+	add_theme_support('post-thumbnails');
 
-		// This theme uses wp_nav_menu() in one location.
-		register_nav_menus(
-			array(
-				'menu-1' => esc_html__( 'Primary', 'melanie_voiture' ),
-			)
-		);
+	// This theme uses wp_nav_menu() in one location.
+	register_nav_menus(array('menu-1'=> esc_html__('Primary', 'melanie_voiture'),
+		));
 
-		/*
+	/*
 		 * Switch default core markup for search form, comment form, and comments
 		 * to output valid HTML5.
 		 */
-		add_theme_support(
-			'html5',
-			array(
-				'search-form',
-				'comment-form',
-				'comment-list',
-				'gallery',
-				'caption',
-				'style',
-				'script',
-			)
-		);
+	add_theme_support('html5',
+		array('search-form',
+			'comment-form',
+			'comment-list',
+			'gallery',
+			'caption',
+			'style',
+			'script',
+		));
 
-		// Set up the WordPress core custom background feature.
-		add_theme_support(
-			'custom-background',
-			apply_filters(
-				'melanie_voiture_custom_background_args',
-				array(
-					'default-color' => 'ffffff',
-					'default-image' => '',
-				)
-			)
-		);
+	// Set up the WordPress core custom background feature.
+	add_theme_support('custom-background',
+		apply_filters('melanie_voiture_custom_background_args',
+			array('default-color'=> 'ffffff',
+				'default-image'=> '',
+			)));
 
-		// Add theme support for selective refresh for widgets.
-		add_theme_support( 'customize-selective-refresh-widgets' );
+	// Add theme support for selective refresh for widgets.
+	add_theme_support('customize-selective-refresh-widgets');
 
-		/**
+	/**
 		 * Add support for core custom logo.
 		 *
 		 * @link https://codex.wordpress.org/Theme_Logo
 		 */
-		add_theme_support(
-			'custom-logo',
-			array(
-				'height'      => 250,
-				'width'       => 250,
-				'flex-width'  => true,
-				'flex-height' => true,
-			)
-		);
-	}
+	add_theme_support('custom-logo',
+		array('height'=> 250,
+			'width'=> 250,
+			'flex-width'=> true,
+			'flex-height'=> true,
+		));
+}
+
 endif;
-add_action( 'after_setup_theme', 'melanie_voiture_setup' );
+add_action('after_setup_theme', 'melanie_voiture_setup');
 
 /**
  * Set the content width in pixels, based on the theme's design and stylesheet.
@@ -112,9 +108,10 @@ add_action( 'after_setup_theme', 'melanie_voiture_setup' );
  * @global int $content_width
  */
 function melanie_voiture_content_width() {
-	$GLOBALS['content_width'] = apply_filters( 'melanie_voiture_content_width', 640 );
+	$GLOBALS['content_width']=apply_filters('melanie_voiture_content_width', 640);
 }
-add_action( 'after_setup_theme', 'melanie_voiture_content_width', 0 );
+
+add_action('after_setup_theme', 'melanie_voiture_content_width', 0);
 
 /**
  * Register widget area.
@@ -122,34 +119,33 @@ add_action( 'after_setup_theme', 'melanie_voiture_content_width', 0 );
  * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
  */
 function melanie_voiture_widgets_init() {
-	register_sidebar(
-		array(
-			'name'          => esc_html__( 'Sidebar', 'melanie_voiture' ),
-			'id'            => 'sidebar-1',
-			'description'   => esc_html__( 'Add widgets here.', 'melanie_voiture' ),
-			'before_widget' => '<section id="%1$s" class="widget %2$s">',
-			'after_widget'  => '</section>',
-			'before_title'  => '<h2 class="widget-title">',
-			'after_title'   => '</h2>',
-		)
-	);
+	register_sidebar(array('name'=> esc_html__('Sidebar', 'melanie_voiture'),
+			'id'=> 'sidebar-1',
+			'description'=> esc_html__('Add widgets here.', 'melanie_voiture'),
+			'before_widget'=> '<section id="%1$s" class="widget %2$s">',
+			'after_widget'=> '</section>',
+			'before_title'=> '<h2 class="widget-title">',
+			'after_title'=> '</h2>',
+		));
 }
-add_action( 'widgets_init', 'melanie_voiture_widgets_init' );
+
+add_action('widgets_init', 'melanie_voiture_widgets_init');
 
 /**
  * Enqueue scripts and styles.
  */
 function melanie_voiture_scripts() {
-	wp_enqueue_style( 'melanie_voiture-style', get_stylesheet_uri(), array(), _S_VERSION );
-	wp_style_add_data( 'melanie_voiture-style', 'rtl', 'replace' );
+	wp_enqueue_style('melanie_voiture-style', get_stylesheet_uri(), array(), _S_VERSION);
+	wp_style_add_data('melanie_voiture-style', 'rtl', 'replace');
 
-	wp_enqueue_script( 'melanie_voiture-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
+	wp_enqueue_script('melanie_voiture-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true);
 
-	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
-		wp_enqueue_script( 'comment-reply' );
+	if (is_singular() && comments_open() && get_option('thread_comments')) {
+		wp_enqueue_script('comment-reply');
 	}
 }
-add_action( 'wp_enqueue_scripts', 'melanie_voiture_scripts' );
+
+add_action('wp_enqueue_scripts', 'melanie_voiture_scripts');
 
 /**
  * Implement the Custom Header feature.
@@ -174,7 +170,6 @@ require get_template_directory() . '/inc/customizer.php';
 /**
  * Load Jetpack compatibility file.
  */
-if ( defined( 'JETPACK__VERSION' ) ) {
+if (defined('JETPACK__VERSION')) {
 	require get_template_directory() . '/inc/jetpack.php';
 }
-
