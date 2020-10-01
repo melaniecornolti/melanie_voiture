7e458612b7643b205df3d1117c2fc4d2c3916553
Cornolti <melanie.cornolti@orange.fr>
2020-10-01 10:13:56 +0200

    Initialisation de single-voiture.php
    Page d'affichage pour une seule voiture

---
+++ single-voiture.php
@@ -0,0 +1,26 @@
+<?php get_header(); ?>
+<div class="voitures">
+    <?php if (have_posts()) : ?>
+    <?php while (have_posts()) : the_post(); ?>
+    <article class="voiture">
+        <?php the_post_thumbnail( 'thumbnail' ); ?>
+        <h1 class="title">
+            <a href="<?php the_permalink(); ?>">
+                <?php the_title(); ?>
+            </a>
+        </h1>
+        <div class="content">
+            <?php the_content(); ?>
+        </div>
+        <div class="caracteristiques">
+            <p>Couleur : <?php the_field( 'couleur' ); ?> • Cylindrée : <?php the_field( 'cylindree' ); ?> • Poids : <?php the_field( 'poids' ); ?></p>
+        </div>
+        <div>
+            <?php the_terms( $post->ID, 'marque', 'Marque : ' ); ?>
+        </div>
+
+    </article>
+    <?php endwhile; ?>
+    <?php endif; ?>
+</div>
+<?php get_footer(); ?>
